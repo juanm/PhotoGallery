3a3884c42b83310d5af85298741cb34a8b7bceac
Juan <juan@freshbooks.com>
2012-08-02 11:07:39 -0400

    Explaining south, added visible_by column to Photo

--- freshphotos/photos/models.py
+++ freshphotos/photos/models.py
@@ -15,10 +15,18 @@ class Gallery(models.Model):
 
 
 class Photo(models.Model):
+
+    VISIBLE_BY_CHOICES = (
+        (u'AD', u'Admin'),
+        (u'LI', u'Logged In Users'),
+        (u'EV', u'Everybody'),
+    )
+
     name = models.CharField(max_length=200)
     filename = models.FileField(upload_to='photos/')
     created = models.DateTimeField(auto_now_add=True)
     galleries = models.ManyToManyField('photos.Gallery')
+    visible_by = models.CharField(max_length=2, choices=VISIBLE_BY_CHOICES, default=u'AD')
 
     class Meta:
         verbose_name_plural  = 'photos'
--- freshphotos/photos/views.py
+++ freshphotos/photos/views.py
@@ -1,20 +1,33 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
 
 from models import Gallery
 from models import Photo
+from forms import PhotoForm
   
-def gallery(request, gallery_id, view='grid'):
+def gallery(request, gallery_id):
     try:
         gallery = Gallery.objects.get(id=gallery_id)
     except Gallery.DoesNotExist:
         raise Http404
-    context = {'gallery': gallery}
-    if view == 'carousel':
-        return render(request, "gallery-detail.html", context)
 
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_photo = form.save(commit=False)
+            new_photo.visible_by = 'AD'
+            new_photo.save()
+            form.save_m2m()
+            return HttpResponseRedirect("/gallery/%s/" % gallery_id)
+    else:
+        form = PhotoForm()
+        context = {'gallery': gallery,
+               'form' : form}
+    
     return render(request, "gallery.html", context)
+
 
 def photo(request, photo_id):
     try:
